c32e5f65065532c9dba2914c5220aa03ed5a6060
Nick Sarbicki <nicksarbicki@toaster.co>
2019-03-13 11:42:26 +0000

    🚀🚀 Initial Commit 🚀🚀

---
+++ secure_scaffold/constants.py
@@ -0,0 +1,4 @@
+USER_ADMIN_HEADER = "X-Appengine-User-Is-Admin"
+USER_AUTH_DOMAIN_HEADER = "X-Appengine-Auth-Domain"
+USER_EMAIL_HEADER = "X-Appengine-User-Email"
+USER_ID_HEADER = "X-Appengine-User-Id"
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import setuptools
+
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+
+setuptools.setup(
+    name="Secure Scaffold",
+    version=0.1,
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    author="Toaster Ltd.",
+    author_email="Toaster Ltd.",
+    description="Secure Scaffold for Google App Engine",
+)
