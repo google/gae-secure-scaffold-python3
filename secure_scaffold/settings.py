fda1cf70ebe1c550025f6fc948fa14d9ee5c2815
Nick Sarbicki <nicksarbicki@toaster.co>
2019-05-15 16:43:38 +0100

    fix(CSP Headers): CSP Headers are invalid - remove offencing characters.
    CSP headers are invalid due to excess quotes and unsafe base64 values.
    
    Whilst base64 values are recommended for CSP NONCE values Python
    adds extra "=" signs onto the value as padding. It is necessary to
    remove these and provide URL Safe values.
    
    As CSP NONCE values are primarily for allowing scripts through with
    the NONCE, this has been added as a setting as well to make it more
    accessible.

--- secure_scaffold/settings.py
+++ secure_scaffold/settings.py
@@ -5,14 +5,24 @@ import os
 NONCE_LENGTH = 10
 
 
-def generate_nonce():
-    return base64.b64encode(os.urandom(NONCE_LENGTH))
+def generate_nonce() -> str:
+    """
+    Generate a nonce for CSP purposes.
 
+    Python URL Safe base64 encoding adds "=" as padding
+    to the text - this is unsafe for CSP purposes so we strip
+    it out.
+    """
+    b64_str = base64.urlsafe_b64encode(os.urandom(NONCE_LENGTH))
+    return b64_str.decode().rstrip('=')
+
+
+CSP_NONCE = generate_nonce()
 
 CSP_CONFIG = {
     'base-uri': "'self'",
     'object-src': "'none'",
-    'script-src': f"'nonce-{generate_nonce()}' 'strict dynamic' 'unsafe-inline' https: http:",
+    'script-src': f"nonce-{CSP_NONCE} strict dynamic 'unsafe-inline' https: http:",
     'report-uri': '/csp/',
 }
 
