03c336bb3ec21c75d6780e73946e49778344036b
Oliver <oliver@toaster.co>
2019-08-26 10:46:23 +0800

    chore() bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Toaster Secure Scaffold RC",
-    version='0.8.1',
+    version='0.8.2',
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     extras_require={
